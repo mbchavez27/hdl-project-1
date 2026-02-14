ce5927e48102dbb3f143f0b01d7caf5d16651936
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2026-02-14 20:47:58 +0800

    feat: add behavioral circuit test bench

--- RChaM.v
+++ RChaM.v
@@ -1,6 +1,6 @@
 `timescale  1ns /1ps
 
-module circuit_behavior(F,A,B,C,D):
+module circuit_behavior(F,A,B,C,D);
     input A,B,C,D;
     output reg F; 
 
@@ -10,7 +10,7 @@ module circuit_behavior(F,A,B,C,D):
             4'd0, 4'd1, 4'd2, 4'd8, 4'd10, 4'd12, 4'd14:
                 F = 1'b0;
             default:
-                F = 1'b1
+                F = 1'b1;
         endcase
     end 
 endmodule
--- RChaM_tb.v
+++ RChaM_tb.v
@@ -1,12 +1,12 @@
 `timescale 1ns / 1ps 
 
 
-module behavior_model_tb():
+module behavior_model_tb();
     reg[3:0] t_input; 
     wire t_F;
     integer i; 
 
-    circuit_behavior(t_F, t_input[3], t_input[2], t_input[1], t_input[0]);
+    circuit_behavior dut (t_F, t_input[3], t_input[2], t_input[1], t_input[0]);
 
     initial begin
         t_input = 4'b0000;
@@ -19,8 +19,7 @@ module behavior_model_tb():
 
     initial begin
          $display("Written by Max Benedict Chavez");
-         $monitor("$monitor("time = %0d ", $time, "A = %b B = %b C = %b D = %b output_F = %b", 
-                     t_input[3], t_input[2], t_input[1], t_input[0], t_F);")
+         $monitor("time = %0d ", $time, "A = %b B = %b C = %b D = %b output_F = %b",  t_input[3], t_input[2], t_input[1], t_input[0], t_F);
          $dumpfile("RChaM.vcd");
          $dumpvars();
     end
