42c39b614084314d5998c93d226e5124b980851a
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2026-02-14 20:40:58 +0800

    feat: add behavioral circuit test bench

--- RChaM_tb.v
+++ RChaM_tb.v
@@ -6,3 +6,24 @@ module behavior_model_tb():
     wire t_F;
     integer i; 
 
+    circuit_behavior(t_F, t_input[3], t_input[2], t_input[1], t_input[0]);
+
+    initial begin
+        t_input = 4'b0000;
+
+        for(i = 1; i<16; i=i+1)
+            begin 
+                #30 t_input = i;
+            end 
+    end
+
+    initial begin
+         $display("Written by Max Benedict Chavez");
+         $monitor("$monitor("time = %0d ", $time, "A = %b B = %b C = %b D = %b output_F = %b", 
+                     t_input[3], t_input[2], t_input[1], t_input[0], t_F);")
+         $dumpfile("RChaM.vcd");
+         $dumpvars();
+    end
+
+endmodule
+
